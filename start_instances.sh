592783b2baaa1c2628729d88bd77f3f3390b150f
H Sareen Kumar <sareenh10@gmail.com>
2025-01-17 17:58:31 +0530

    Create start_instances.sh

---
+++ start_instances.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+INSTANCE_ID=$1
+if [ -z "$INSTANCE_ID" ]; then
+    echo "Usage: $0 <INSTANCE_ID>"
+    exit 1
+fi
+
+aws ec2 start-instances --instance-ids $INSTANCE_ID --output table
+echo "Instance $INSTANCE_ID started."
