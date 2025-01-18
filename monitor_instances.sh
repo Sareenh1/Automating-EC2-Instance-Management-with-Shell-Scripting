80aba16fffbb386726c8cde0b7726b234cd6d67a
H Sareen Kumar <sareenh10@gmail.com>
2025-01-18 19:33:07 +0530

    Update monitor_instances.sh

--- monitor_instances.sh
+++ monitor_instances.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,State.Name,PublicIpAddress]' --output table
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[12345678 instance id,State.Name,PublicIpAddress]' --output table
